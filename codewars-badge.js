f225ab4ad1416787bc258e7210d15e90e8913a42
Saliha Popal <salihapopal@gmail.com>
2023-06-02 13:20:36 +0100

    Done, but lighthouse is not working.

--- codewars-badge.js
+++ codewars-badge.js
@@ -4,14 +4,25 @@
 
 class CodeWarsBadge extends HTMLElement {
   constructor() {
+    // Always call super first in constructor
     super();
+    // Create a shadow root
     this.attachShadow({ mode: "open" });
-    this.userName = "CodeYourFuture";
+    this.userName = "SalihaPopal";
     this.userData = [];
   }
-
+//The actual updates are all handled by the life cycle callbacks, which are placed inside the class definition as methods. The connectedCallback() runs each time the element is added to the DOM 
   connectedCallback() {
     this.fetchActivity()
+      .then(() => {
+        this.render();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+  connectedCallback() {
+    this.fetchUserDetails()
       .then(() => {
         this.render();
       })
@@ -23,10 +34,25 @@ class CodeWarsBadge extends HTMLElement {
   // fetch the data from the Codewars API
   async fetchActivity() {
     const response = await fetch(
-      `https://www.codewars.com/api/v1/users/${this.userName}`
+      `https://www.codewars.com/api/v1/users/${this.SalihaPopal}`
     );
     const data = await response.json();
     this.userData = data; // set the userData property with the fetched data
+  }
+
+  async fetchUserDetails() {
+    const response = await fetch(
+      `https://www.codewars.com/api/v1/users/SalihaPopal`
+    );
+    const data = await response.json();
+    this.userData = data;
+  }
+
+  async fetchCodeChallenges() {
+    const response = await fetch(`https://www.codewars.com/api/v1/challenges`
+    );
+    const data = await response.json();
+    this.codeChallenges = data; 
   }
 
   render() {
@@ -40,11 +66,43 @@ class CodeWarsBadge extends HTMLElement {
             color: var(--rank);
             border: 3px solid; 
             padding: .25em .5em;
-        }      
+        }
+        user{
+          line-height: 1.5;
+        } 
+        honor, clan, leaderboardPosition, skills, ranks, codeChallenges
+        {
+          background-color: rgba(0,0,0,0.05);
+    padding: 3px;
+    border-radius: 3px;
+        }
+        
       </style>
         <data value="${this.userData.ranks.overall.score}">
         ${this.userData.ranks.overall.name}
-        </data>`;
+        </data>
+        <user value="${this.userData.name}">
+        ${this.userData.name}</user><hr>
+
+        <honor value ="${this.userData.honor}">
+        Honor : ${this.userData.honor}</honor><hr>
+
+        <clan value ="${this.userData.clan}"> 
+        Clan:${this.userData.clan}</clan><hr>
+
+        <leaderboardPosition value ="${this.userData.clan}"> 
+        LeaderboardPosition:${this.userData.leaderboardPosition}</leaderboardPosition><hr>
+
+        <skills value ="${this.userData.skills}"> 
+        Skills:${this.userData.skills}</skills><hr>
+
+        <ranks value ="${this.userData.ranks}"> 
+        Ranks:${this.userData.ranks}</ranks><hr>
+
+        
+        <challenge value ="${this.userData.codeChallenges.totalCompleted}">
+        Total completed kata : ${this.userData.codeChallenges.totalCompleted}<hr>
+        `
   }
 }
 
