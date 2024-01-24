28eaff53c5a3d5198c647ef618c252af3fbb2c7d
karimi <behrouzkarimi1993@gmail.com>
2024-01-24 19:00:03 +0000

    create a new tag and setting it up

--- codewars-badge.js
+++ codewars-badge.js
@@ -49,3 +49,118 @@ class CodeWarsBadge extends HTMLElement {
 }
 
 customElements.define("codewars-badge", CodeWarsBadge);
+
+const template = document.createElement("template");
+template.innerHTML = `
+<div class='user-card'>
+<h3></h3>
+<p id='user-name'></p>
+<p id='name-of-user'></p>
+<ul id='skills></ul>
+<p id='clan'></p>
+<p id='languages'></p>
+</div>
+
+<style>
+.user-card{
+  background-color:#1f1f2e;
+  color:white;
+  margin:2rem auto 2rem auto;
+  width:60%
+}
+h3{
+  margin:0;
+  padding:0;
+  
+  
+}
+</style>
+`;
+
+class SelfCreated extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.userName = "bkarimii";
+    this.userData = [];
+  }
+
+  connectedCallback() {
+    this.fetching()
+      .then(() => this.render())
+      .then(() => console.log("this is userData------->", this.userData))
+      .then(console.log(this.userData.ranks), "<------------second console")
+      .catch(error, console.log(error, "<---------- error happened"));
+  }
+  async fetching() {
+    const response = await fetch(
+      `https://www.codewars.com/api/v1/users/${this.userName}`
+    );
+
+    const data = await response.json();
+    this.userData = data;
+  }
+
+  render() {
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector("h3").textContent = "The User Profile:";
+    this.shadowRoot.querySelector(
+      "#user-name"
+    ).textContent = `USER NAME: ${this.userData.username}`;
+    this.shadowRoot.querySelector(
+      "#name-of-user"
+    ).textContent = `Name: ${this.userData.name}`;
+    //`${this.userData.ranks.overall.color}`;
+  }
+}
+
+window.customElements.define("self-created", SelfCreated);
+
+const obj = {
+  username: "some_user",
+  name: "Some Person",
+  honor: 544,
+  clan: "some clan",
+  leaderboardPosition: 134,
+  skills: [
+    "ruby",
+    "c#",
+    ".net",
+    "javascript",
+    "coffeescript",
+    "nodejs",
+    "rails",
+  ],
+  ranks: {
+    overall: {
+      rank: -3,
+      name: "3 kyu",
+      color: "blue",
+      score: 2116,
+    },
+    languages: {
+      javascript: {
+        rank: -3,
+        name: "3 kyu",
+        color: "blue",
+        score: 1819,
+      },
+      ruby: {
+        rank: -4,
+        name: "4 kyu",
+        color: "blue",
+        score: 1005,
+      },
+      coffeescript: {
+        rank: -4,
+        name: "4 kyu",
+        color: "blue",
+        score: 870,
+      },
+    },
+  },
+  codeChallenges: {
+    totalAuthored: 3,
+    totalCompleted: 230,
+  },
+};
